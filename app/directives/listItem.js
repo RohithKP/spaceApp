1cf872628039d0c6e008e3cc6672015589093797
RohithKP <rohithkp.me@gmail.com>
2016-02-17 19:15:52 +0530

    added dashboard and routing

--- app/directives/listItem.js
+++ app/directives/listItem.js
@@ -15,6 +15,7 @@ angular.module('myApp').directive('myNotebook', function ($filter) {
                  },
                 link:function (scope, element, attrs) {
              		  scope.$watch('todos', function () {
+                  console.log(scope.todos);
                   scope.remainingCount = $filter('filter')(scope.todos, { done: false }).length;
                   scope.completedCount = scope.todos.length - scope.remainingCount;
                   scope.allChecked = !scope.remainingCount;
@@ -42,4 +43,35 @@ angular.module('myApp').directive('myNotebook', function ($filter) {
                 }); */
             }
         };
-    })
+    }).directive('contenteditable', function() {
+     return {
+      controller:function ($scope, $attrs) {
+          $scope.bool = 'false'
+                      $scope.edit = function (id) {
+                       $scope.bool = true;
+//                       $(angular.element( document.querySelector( '.todo-text' ) )[0]).addClass('editing');
+                           }
+      },
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      // view -> model
+      elm.on('blur', function() {
+        elm.removeClass('editing');
+        ctrl.$setViewValue(elm.html());
+       scope.bool =false;
+      });
+      elm.on('focus', function() {
+        console.log("foused");
+        elm.addClass('editing');
+      });
+
+      // model -> view
+      ctrl.$render = function() {
+        elm.html(ctrl.$viewValue);
+      };
+
+      // load init value from DOM
+      ctrl.$setViewValue(elm.html());
+    }
+  };
+});
--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,25 @@
-angular.module('myApp', ['angular.filter']);
+angular.module('myApp', ['angular.filter','ngRoute']);
+
+    // configure our routes
+    angular.module('myApp').config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/todo', {
+                templateUrl : '../partials/todo.html',
+                controller  : 'TodoCtrl'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'contactController'
+            });
+    });
 
