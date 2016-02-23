5a3f15d24f42bd0e0ad12dc7e769fff381991975
RohithKP <rohithkp.me@gmail.com>
2016-02-23 10:45:22 +0530

    solved file not found errord

--- app/js/app.js
+++ app/js/app.js
@@ -6,19 +6,19 @@ angular.module('myApp', ['angular.filter','ngRoute','ngAnimate']);
 
             // route for the home page
             .when('/todo', {
-                templateUrl : '../partials/todo.html',
+                templateUrl : './partials/todo.html',
                 controller  : 'TodoCtrl'
             })
 
             // route for the about page
             .when('/customers', {
-                templateUrl : '../partials/customers.html',
+                templateUrl : './partials/customers.html',
                 controller  : ''
             })
 
             // route for the contact page
             .when('/dashboard', {
-                templateUrl : '../partials/dashboard.html',
+                templateUrl : './partials/dashboard.html',
                 controller  : ''
             });
     });
