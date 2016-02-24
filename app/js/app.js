2e7d99c6103252c0577f218601705873b20dbade
RohithKP <rohithkp.me@gmail.com>
2016-02-24 11:43:34 +0530

    path changes

--- app/js/app.js
+++ app/js/app.js
@@ -6,7 +6,7 @@ angular.module('myApp', ['angular.filter','ngRoute','ngAnimate']);
 
             // route for the home page
             .when('/todo', {
-                templateUrl : './partials/todo.html',
+                templateUrl : './app/partials/todo.html',
                 controller  : 'TodoCtrl'
             })
             .when('/customer/:custId', {
