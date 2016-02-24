637ca280ac28d2388674e6b894c7d260d45fa2b3
RohithKP <rohithkp.me@gmail.com>
2016-02-24 11:41:57 +0530

    path changes

--- app/js/app.js
+++ app/js/app.js
@@ -6,26 +6,26 @@ angular.module('myApp', ['angular.filter','ngRoute','ngAnimate']);
 
             // route for the home page
             .when('/todo', {
-                templateUrl : '/partials/todo.html',
+                templateUrl : './partials/todo.html',
                 controller  : 'TodoCtrl'
             })
             .when('/customer/:custId', {
-                templateUrl: '/partials/customerDetails.html',
+                templateUrl: './partials/customerDetails.html',
                 controller: 'customerDetailsCtrl'
             })
 
             // route for the about page
             .when('/customers', {
-                templateUrl : '/partials/customers.html',
+                templateUrl : './partials/customers.html',
                 controller  : 'customerCtrl'
             })
 
             // route for the contact page
             .when('/dashboard', {
-                templateUrl : '/partials/dashboard.html',
+                templateUrl : './partials/dashboard.html',
                 controller  : ''
             })
             .otherwise({
-            redirectTo: '/index.html'
+            redirectTo: '/dashboard'
             });
     });
