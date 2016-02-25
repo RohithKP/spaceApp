07a0ee915c1c31f2837f73b90baa02828d8bf446
RohithKP <rohithkp.me@gmail.com>
2016-02-25 13:16:35 +0530

    created gh-apges

---
+++ app/controllers/customerCtrl.js
@@ -0,0 +1,33 @@
+angular.module('myApp').controller('customerCtrl', ['$scope', '$http', 'jsonService', function($scope, $http, jsonService) {
+
+    $scope.pageClass = 'page-customer';
+    $scope.display = 'false';
+    $scope.toggleShow = function() {
+        $scope.display = !$scope.display;
+
+    }
+
+    $scope.customers = [];
+    var fetch = jsonService.fetchAllJson("customers");
+    fetch.success(function(data) {
+        $scope.customers = data;
+    });
+
+
+    // $scope.saved = localStorage.getItem('customers');
+    // $scope.customers = $scope.customers.push($scope.saved);
+    // $scope.customers = ($scope.saved !== null) ? JSON.parse($scope.saved) : [];
+
+    $scope.addCustomer = function() {
+        $scope.customers.unshift({
+            custName: $scope.custName,
+            address: $scope.address,
+            country: $scope.country,
+            accMngr: $scope.accMngr,
+            accMngrEmail: $scope.accMngrEmail
+        });
+        // localStorage.setItem('customers', JSON.stringify($scope.customers));
+
+    };
+
+}]);
---
+++ readme.txt
@@ -0,0 +1,1 @@
+AngularJS 'Space' App is a dashboard  that provide information about employee,customer, projects, vacation details, status of employees, and to do list.
